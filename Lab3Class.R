c4641ea7d52d0e5ef7384f74d26dcb06cfb0336d
Pablo <pablovaldes@gmail.com>
2024-05-15 11:57:32 -0400

    Update Lab3Class.R
    Just an idea :)

--- Lab3Class.R
+++ Lab3Class.R
@@ -5,3 +5,5 @@ library('tidyverse')
 # Download data to R
 new_data <- read.csv("/Volumes/PVD2/1_Montreal/1_UdeM/3_Courses/Animal_Data_Science/data/test_.csv")
 
+# I have a new idea!
+summary(new_data)
